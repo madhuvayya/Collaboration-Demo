b6476631cd81736b2c583d99d99e1583f914ed73
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-29 17:18:01 +0530

    Added:try to add in development

--- src/Demo.java
+++ src/Demo.java
@@ -2,7 +2,7 @@ public class Demo {
 
     public static void main(String[] args) {
         String name = "Demo";
-        System.out.println("-----start-------");
+        System.out.println("-----Start-------");
         System.out.println(name);
         System.out.println("exit");
         System.out.println("Hello World !! "+name);
