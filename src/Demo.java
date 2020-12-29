c319c39765e47b1802b318b76c045a9bc66bd9a1
dvranga <dvranga@users.noreply.github.com>
2020-12-29 16:26:02 +0530

    [ranganath]Added:Refactore

--- src/Demo.java
+++ src/Demo.java
@@ -1,8 +1,6 @@
 public class Demo {
 
     public static void main(String[] args) {
-        User user = new User();
-        user.setName("Demo");
-        System.out.println("Name"+user.getName());
+        System.out.println("Name");
     }
 }
