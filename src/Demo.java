b2b988666f44fbbf80a2e7abedf48195b14c54bc
dvranga <dvranga@users.noreply.github.com>
2020-12-29 17:14:44 +0530

    [added the code in demo]

--- src/Demo.java
+++ src/Demo.java
@@ -2,9 +2,10 @@ public class Demo {
 
     public static void main(String[] args) {
         String name = "Demo";
+        System.out.println("-----start-------");
         System.out.println(name);
         System.out.println("exit");
-        System.out.println("Hello World !!");
+        System.out.println("Hello World !! "+name);
         System.out.println("-----End-------");
     }
 }
